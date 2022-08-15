56e584e2b00eeffe44df8b0d838181e67e188806
JeremyRivera <fangman09@gmail.com>
2022-08-15 17:44:16 -0400

    project 4 stage 1 array

---
+++ scripts/index.js
@@ -0,0 +1,63 @@
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg ",
+  },
+];
+// const profileEditButton = document.querySelector("profile_edit-button");
+// const profileEditPopup = document.querySelector(".popup");
+// const profileEditCloseButton = document.querySelector(".popup__close");
+// const profileEditForm = document.querySelector("#edit-profile-form");
+// const profileTitleEl = document.querySelector(".profile_title");
+// const profileDescriptionEl = document.querySelector(".profile_description");
+// const profileTitleInput = profileEditForm.querySelector(
+//   ".popup__input_type_name"
+// );
+// const profileDescriptionInput = profileEditForm.querySelector(
+//   ".popup_input_type_ description"
+// );
+
+// function closePopup() {
+//   profileEditPopup.classList.remove("popup_is-opened");
+// }
+
+// function openPopup() {
+//   profileEditPopup.classList.add("popup_is-opened");
+// }
+
+// profileEditButton.addEventListener("click", () => {
+//   profileTitleInput.value = profileTitleEl.textContent;
+//   profileDescriptionInput.value = profileDescriptionEl.textContent;
+//   openPopup();
+// });
+// profileEditCloseButton.addEventListener("click", closePopup);
+// I;
+// profileEditForm.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const titleValue = profileTitleInput.value;
+//   const descriptionValue = profileDescriptionInput.value;
+
+//   profileTitleEl.textContent = titleValue;
+//   profileDescriptionEl.textContent = descriptionValue;
+//   closePopup();
+// });
