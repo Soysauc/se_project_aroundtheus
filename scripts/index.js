20735280dbdd6c3a76bada37582f95e2ec18239a
JeremyRivera <fangman09@gmail.com>
2022-08-17 15:15:25 -0400

    close button functionality

--- scripts/index.js
+++ scripts/index.js
@@ -24,40 +24,40 @@ const initialCards = [
     link: "https://code.s3.yandex.net/web-code/lago.jpg ",
   },
 ];
-// const profileEditButton = document.querySelector("profile_edit-button");
-// const profileEditPopup = document.querySelector(".popup");
-// const profileEditCloseButton = document.querySelector(".popup__close");
-// const profileEditForm = document.querySelector("#edit-profile-form");
-// const profileTitleEl = document.querySelector(".profile_title");
-// const profileDescriptionEl = document.querySelector(".profile_description");
-// const profileTitleInput = profileEditForm.querySelector(
-//   ".popup__input_type_name"
-// );
-// const profileDescriptionInput = profileEditForm.querySelector(
-//   ".popup_input_type_ description"
-// );
+const profileEditButton = document.querySelector(".profile__edit-button");
+const profileEditPopup = document.querySelector(".popup");
+const profileEditCloseButton = document.querySelector(".popup__close");
+const profileEditForm = document.querySelector("#edit-profile-form");
+const profileTitleEl = document.querySelector(".profile__title");
+const profileDescriptionEl = document.querySelector(".profile__description");
+const profileTitleInput = profileEditForm.querySelector(
+  ".popup__input_type_name"
+);
+const profileDescriptionInput = profileEditForm.querySelector(
+  ".popup__input_type_description"
+);
 
-// function closePopup() {
-//   profileEditPopup.classList.remove("popup_is-opened");
-// }
+function closePopup() {
+  profileEditPopup.classList.remove("popup_is-opened");
+}
 
-// function openPopup() {
-//   profileEditPopup.classList.add("popup_is-opened");
-// }
+function openPopup() {
+  profileEditPopup.classList.add("popup_is-opened");
+}
 
-// profileEditButton.addEventListener("click", () => {
-//   profileTitleInput.value = profileTitleEl.textContent;
-//   profileDescriptionInput.value = profileDescriptionEl.textContent;
-//   openPopup();
-// });
-// profileEditCloseButton.addEventListener("click", closePopup);
-// I;
-// profileEditForm.addEventListener("submit", (event) => {
-//   event.preventDefault();
-//   const titleValue = profileTitleInput.value;
-//   const descriptionValue = profileDescriptionInput.value;
+profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitleEl.textContent;
+  profileDescriptionInput.value = profileDescriptionEl.textContent;
+  openPopup();
+});
+profileEditCloseButton.addEventListener("click", closePopup);
 
-//   profileTitleEl.textContent = titleValue;
-//   profileDescriptionEl.textContent = descriptionValue;
-//   closePopup();
-// });
+profileEditForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const titleValue = profileTitleInput.value;
+  const descriptionValue = profileDescriptionInput.value;
+
+  profileTitleEl.textContent = titleValue;
+  profileDescriptionEl.textContent = descriptionValue;
+  closePopup();
+});
