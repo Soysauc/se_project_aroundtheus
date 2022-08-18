ad5bbcad0f4491f1187856bbef6e24875d4594bc
JeremyRivera <fangman09@gmail.com>
2022-08-18 16:02:19 -0400

    troubleshooting cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,11 @@
-const initialCards = [
-  {
-    name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
-  },
-  {
-    name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
-  },
-  {
-    name: "Vanoise National Park",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg ",
-  },
-];
+//---------------------------------------------------------
+//Popup content
+//---------------------------------------------------------
 const profileEditButton = document.querySelector(".profile__edit-button");
 const profileEditPopup = document.querySelector(".popup");
 const profileEditCloseButton = document.querySelector(".popup__close");
 const profileEditForm = document.querySelector("#edit-profile-form");
+const editForm = document.querySelector(".popup__form");
 const profileTitleEl = document.querySelector(".profile__title");
 const profileDescriptionEl = document.querySelector(".profile__description");
 const profileTitleInput = profileEditForm.querySelector(
@@ -61,3 +39,111 @@ profileEditForm.addEventListener("submit", (event) => {
   profileDescriptionEl.textContent = descriptionValue;
   closePopup();
 });
+//---------------------------------------------------------
+//as per cards thus follows
+//---------------------------------------------------------
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg ",
+  },
+];
+initialCards.forEach(function (cardData) {
+  // clone template
+  const cardEl = cardTemplate.cloneNode(true);
+  // find .card__image
+  const imageEl = cardEl.querySelector(".card__image");
+  // find card title
+  const cardTitle = cardEl.querySelector(".card__text");
+  // replace image sre
+  imageEl.src = cardData.link;
+  // replace image alt
+  imageEl.alt = cardData.name;
+  // replace title
+  cardTitle.textContent = cardData.name;
+  // append to list
+  cardListEl.appendChild(cardEl);
+});
+// const cardTemplate = document
+//   .querySelector("#card-template")
+//   .content.querySelector(".card__item");
+// const cardsList = document.querySelector(".card__list");
+
+// function generateCard(card) {
+//   const cardElement = cardTemplate.cloneNode(true);
+//   cardElement.querySelector(".card__text").textContent = card.name;
+//   const imageEl = cardElement.querySelector(".card__image");
+//   imageEl.style.backgroundImage = `url(${card.link})`;
+
+//   imageEl.addEventListener("click", function () {
+//     previewImgCloseup.src = card.link;
+//     toggleModalWindow(preViewImgModalWindow);
+//   });
+
+//   return cardElement;
+// }
+
+// function renderCard(card, container) {
+//   container.append(card);
+// }
+//---------------------------------------------------------
+// templates
+//---------------------------------------------------------
+
+editForm.addEventListener("submit", formSubmitHandler);
+profileEditButton.addEventListener("click", () =>
+  toggleModalWindow(editModalWindow)
+);
+profileAddButton.addEventListener("click", () =>
+  toggleModalWindow(addModalWindow)
+);
+editModalCloseButton.addEventListener("click", () =>
+  toggleModalWindow(editModalWindow)
+);
+addModalCloseButton.addEventListener("click", () =>
+  toggleModalWindow(addModalWindow)
+);
+ImgCloseupModalCloseButton.addEventListener("click", () =>
+  toggleModalWindow(preViewImgModalWindow)
+);
+function renderCard(card, container) {
+  container.append(card);
+}
+initialCards.forEach(function (card) {
+  const newCard = generateCard(card);
+  renderCard(newCard, cardList);
+});
+
+initialCards.forEach(function (card) {
+  const newCard = generateCard(card);
+  renderCard(newCard, cardList);
+});
+
+// method for adding the child in the list beggining is .prepend
+/*cardsarray.forEach(item => {
+  const template = document.query...('.template');
+  const clone = template.clone();
+  clone.query...(img).src = item.link
+  prepend
+
+});*/
