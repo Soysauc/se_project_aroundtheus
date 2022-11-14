575566310273e6ca67798dcfffb5c734002c301f
Jeremy M Rivera <fangman09@gmail.com>
2022-11-13 19:29:41 -0500

    api code good

--- src/components/Api.js
+++ src/components/Api.js
@@ -32,7 +32,21 @@ export default class Api {
         console.log(err); // log the error to the console
       });
   }
+  //--
+  updateProfile(data) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._authToken,
+      body: JSON.stringify({
+        name: data.name,
+        about: data.about,
+      }),
+    }).then((res) => {
+      res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+    });
+  }
 
+  //--
   loadCard({ name, link }) {
     return fetch(`${this._baseUrl}/cards`, {
       method: "POST",
@@ -44,6 +58,53 @@ export default class Api {
         name,
         link,
       }),
+    });
+  }
+  //--
+  deleteCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: "DELETE",
+      headers: this._authToken,
+    }).then((res) => {
+      res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+    });
+  }
+
+  updateAvatar(data) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: "PATCH",
+      headers: this._authToken,
+      body: JSON.stringify({
+        avatar: data.link,
+      }),
+    }).then((res) => {
+      res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+    });
+  }
+
+  getLikes(cardId) {
+    return fetch(`${this._baseUrl}/cards/likes${cardId}`, {
+      headers: this._authToken,
+    }).then((res) => {
+      res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+    });
+  }
+
+  addLike(cardId) {
+    return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+      headers: this._authToken,
+      method: "PUT",
+    }).then((res) => {
+      res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+    });
+  }
+
+  removeLike(cardId) {
+    return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+      headers: this._authToken,
+      method: "DELETE",
+    }).then((res) => {
+      res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
     });
   }
 }
